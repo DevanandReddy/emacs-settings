845768c428454c9e3d04a0ec54befea6d059f873
Gangi Reddy, Devanand <g.devanandreddy@gmail.com>
2016-09-01 22:30:42 -0500

    elisp customizations,registers.

---
+++ emacs.d/lisp/registers.el
@@ -0,0 +1,7 @@
+;;; package --- Summary
+;;; Code:
+;;; Commentary:
+(set-register ?t (cons 'file "~/Dropbox/ConcurBookKeeping/conquest-time-tracking.org"))
+(set-register ?s (cons 'file "~/Dropbox/ConcurBookKeeping/concur-secrets.org"))
+(provide 'registers)
+;;; registers.el ends here
